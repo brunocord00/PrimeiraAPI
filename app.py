c82a43b6954fb86434a76501533e80e9c2d0056e
Bruno Cordeiro <brunocord00@live.com>
2023-03-07 15:57:41 -0300

    Commit inicial

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask("__name__")
+
+frutinhas = []
+
+@app.get("/criarfrutas/nome=<nomeFruta>")
+def criarfruta(nomeFruta):
+    frutinhas.append(nomeFruta)
+    return frutinhas
+
+@app.get("/verfrutas")
+def ListarFrutas():
+    return frutinhas
+
+@app.get("/verfruta/id=<idfruta>")
+def PegarFruta(idfruta):
+    try:
+        idfruta = int(idfruta)
+        return frutinhas[idfruta]
+    except:
+        return "Fruta não existe."
+    
+app.run(port=6642)
